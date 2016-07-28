4e7c4f4181016fd34c5b19b8850409a1183db75b
AnuarBJJ <anuar.zhukenov@gmail.com>
2016-07-28 13:15:24 -0700

    Initial Commit

---
+++ streetWho/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  streetWho
+//
+//  Created by Anuar Zhukenov on 7/28/16.
+//  Copyright © 2016 Anuar Zhukenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
